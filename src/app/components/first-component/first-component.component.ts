481c3cd1f52f5cd75a2e029d01811bfe691f85b1
Arthur Mascaro <arthumascaro69@gmail.com>
2023-11-22 17:41:54 -0300

    first commit

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements  OnInit {
+
+  name: string = "Arthur";
+  age: number = 30;
+
+
+  constructor(){
+  }
+
+  ngOnInit(): void {
+  }
+
+}
